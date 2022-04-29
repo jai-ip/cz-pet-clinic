77c7dbe910c1acf2c69087df28436d88d171ced8
jaihunter <jayachandranjaicz@gmail.com>
2022-04-29 11:33:54 +0530

    Adding the Project

---
+++ src/main/java/cz/springframework/czpetclinic/CzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package cz.springframework.czpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzPetClinicApplication.class, args);
+	}
+
+}
