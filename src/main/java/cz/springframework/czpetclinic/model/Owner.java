7ce3584cf9524b7bf8defb997eeaa495af64fbe7
jaihunter <jayachandranjaicz@gmail.com>
2022-04-29 19:46:34 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/cz/springframework/czpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package cz.springframework.czpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/cz/springframework/czpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package cz.springframework.czpetclinic.model;
+
+public class Vet extends Person{
+}
