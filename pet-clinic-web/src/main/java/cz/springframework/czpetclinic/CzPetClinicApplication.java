d827e09c8ce3e9fa88c1d1c24646d03a5b797743
jaihunter <jayachandranjaicz@gmail.com>
2022-05-01 19:00:15 +0530

    fixing testing

--- pet-clinic-web/src/main/java/cz/springframework/czpetclinic/CzPetClinicApplication.java
+++ pet-clinic-web/src/main/java/cz/springframework/czpetclinic/CzPetClinicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CzPetClinicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CzPetClinicApplication.class, args);
 	}
