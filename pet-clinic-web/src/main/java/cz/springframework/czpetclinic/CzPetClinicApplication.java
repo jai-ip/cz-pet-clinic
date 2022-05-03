2ee20e766356cf2482956912f96ff4ccb1b9335b
jaihunter <jayachandranjaicz@gmail.com>
2022-05-03 12:55:22 +0530

    Index Page Added. closes #8

--- pet-clinic-web/src/main/java/cz/springframework/czpetclinic/CzPetClinicApplication.java
+++ pet-clinic-web/src/main/java/cz/springframework/czpetclinic/CzPetClinicApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CzPetClinicApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CzPetClinicApplication.class, args);
 	}
