dc3b98029c5d539da584744a2a20c04119fe0613
jaihunter <jayachandranjaicz@gmail.com>
2022-05-04 10:52:54 +0530

    Added Vet Index and Controller. closes #7

---
+++ pet-clinic-web/src/main/java/cz/springframework/czpetclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package cz.springframework.czpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"vets","vets/index","vets/index.html"})
+    public String vetList(){
+
+        return "vets/index";
+    }
+}
