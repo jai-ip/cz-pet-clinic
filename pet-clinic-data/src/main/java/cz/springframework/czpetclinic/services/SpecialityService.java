0d0529b3c8dd5d23314dd889bb89b029eca3c730
jaihunter <jayachandranjaicz@gmail.com>
2022-05-10 14:29:04 +0530

    Added Specialities Service and Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/cz/springframework/czpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package cz.springframework.czpetclinic.services;
+
+import cz.springframework.czpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
